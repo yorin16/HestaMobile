4e1bff64ad809ea6faba16de8e5204ffe2e86d50
Yorin <S1090402@student.windesheim.nl>
2020-05-21 12:30:59 +0200

    meta data toegevoegd voor mobile view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-
+<!--<a class="header-item" href="/#welcome">Welkom</a>
+					<a class="header-item" href="/#tools">Gereedschap</a>
+					<a class="header-item" href="/#fashion">Fashion</a>
+					<a class="header-item" href="/#about">Over ons</a>
+					<a class="header-item" href="/#contact">Contact</a> -->
 <html>
     <head>
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/fullpage.css') }}" />
@@ -15,27 +19,36 @@
 		<script type="text/javascript" src="{{ URL::asset('js/fullpage.js') }}"></script>
 		<script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
 		<script type="text/javascript" src="{{ URL::asset('js/header.js') }}"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
 
     <body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link disabled" href="#">Disabled</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+
         <!-- FULLPAGE section -->
-		<div id="header" class="header">
-            <div class="header-div">
-				<a class="header-image-link"> <img src="{{ asset('/Image/Hesta-logo-2020-white.jpg') }}" class="header-image"> </a>
-				<div class="header-item-container">
-					<a class="header-item" href="/#welcome">Welkom</a>
-					<a class="header-item" href="/#tools">Gereedschap</a>
-					<a class="header-item" href="/#fashion">Fashion</a>
-					<a class="header-item" href="/#about">Over ons</a>
-					<a class="header-item" href="/#contact">Contact</a>
-				</div>
-			</div>
-			<div class="header-line">
-
-			</div>
-        </div>
-
-
         <div id="fullpage">
 
             <!-- WELCOME section -->
